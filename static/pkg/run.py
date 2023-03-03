3e79397d2467191a471404c7936f9b5c9e177759
Jonathan Myers <jon@bridgetoncap.com>
2023-03-03 11:13:18 -0700

    Add working module example

--- static/pkg/run.py
+++ static/pkg/run.py
@@ -1,19 +1,15 @@
-#from js import document
-#import urllib.parse
-#
-#def getAndReturnText():
-#  abctext = document.getElementById("abcinput")
-#  return abctext.value
-#
-#def download(filename, text):
-#  element = document.createElement('a')
-#  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + urllib.parse.urlencode(text))
-#  element.setAttribute('download', filename)
-#  element.style.display = 'none'
-#  document.body.appendChild(element)
-#  element.click()
-#  document.body.removeChild(element)
+from js import document
 import urllib.parse
+
 def getAndReturnText():
-  abctext = document.getElementById("abcinput")
-  return abctext.value
+ abctext = document.getElementById("abcinput")
+ return abctext.value
+
+def download(filename, text):
+ element = document.createElement('a')
+ element.setAttribute('href', 'data:text/plain;charset=utf-8,' + urllib.parse.urlencode(text))
+ element.setAttribute('download', filename)
+ element.style.display = 'none'
+ document.body.appendChild(element)
+ element.click()
+ document.body.removeChild(element)
